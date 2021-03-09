9eea7c130a7af01cd45662429cb0071a6949cb80
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-03-08 20:51:10 -0600

    more gui stuff

--- src/main/java/io/github/thecursedfabricproject/taffeta/config/impl/ConfigScreen.java
+++ src/main/java/io/github/thecursedfabricproject/taffeta/config/impl/ConfigScreen.java
@@ -3,19 +3,25 @@ package io.github.thecursedfabricproject.taffeta.config.impl;
 import java.util.Map;
 
 import io.github.coolmineman.coolconfig.CoolConfigNt;
+import io.github.coolmineman.coolconfig.schema.MapType;
+import io.github.coolmineman.coolconfig.schema.ObjectType;
+import io.github.coolmineman.coolconfig.schema.Type;
 import io.github.coolmineman.nestedtext.api.tree.NestedTextNode;
 import io.github.thecursedfabricproject.taffeta.Taffeta;
 import io.github.thecursedfabricproject.taffeta.config.ModConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.EntryListWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 
-public class ConfigScreen extends Screen {
+public final class ConfigScreen extends Screen {
     private final Screen parent;
     private final ModConfig modConfig;
+    private final ObjectType schema;
+    private NestedTextNode knownGood;
     private NestedTextNode data;
     private ConfigEntryListWidget entryListWidget;
 
@@ -23,18 +29,47 @@ public class ConfigScreen extends Screen {
         super(new LiteralText("Config GUI"));
         this.parent = parent;
         this.modConfig = modConfig;
+        this.schema = this.modConfig.getSchema();
+        this.knownGood = CoolConfigNt.save(modConfig);
         this.data = CoolConfigNt.save(modConfig);
     }
 
     @Override
     protected void init() {
         super.init();
+        rebuild();
+    }
+
+    private void rebuild() {
         this.children.clear();
         entryListWidget = new ConfigEntryListWidget(MinecraftClient.getInstance(), width, height, 32, this.height - 32, 32);
-        for (Map.Entry<String, NestedTextNode> entry : data.asMap().entrySet()) {
-            entryListWidget.children().add(new LeafEntry(entry.getKey(), data));
-        }
+        appendStuff(data.asMap(), 0, true);
         this.addChild(entryListWidget);
+    }
+
+    private void appendStuff(Map<String, NestedTextNode> stuff, int indent, boolean root) {
+        for (Map.Entry<String, NestedTextNode> entry : stuff.entrySet()) {
+            Type type = schema.value.get(entry.getKey());
+            if (type instanceof MapType) {
+                Entry guiEntry = new MapHeadEntry(entry.getKey(), stuff);
+                guiEntry.indent = indent;
+                entryListWidget.children().add(guiEntry);
+                appendStuff(entry.getValue().asMap(), indent + 1, false);
+                Entry guiEntry2 = new AppendMapButton(entry.getValue().asMap());
+                guiEntry2.indent = indent;
+                entryListWidget.children().add(guiEntry2);
+            } else {
+                if (root) {
+                    Entry guiEntry = new LeafEntry(entry.getKey(), stuff);
+                    guiEntry.indent = indent;
+                    entryListWidget.children().add(guiEntry);
+                } else {
+                    Entry guiEntry = new KVEntry(entry.getKey(), stuff);
+                    guiEntry.indent = indent;
+                    entryListWidget.children().add(guiEntry);
+                }
+            }
+        }
     }
 
     @Override
@@ -52,6 +87,9 @@ public class ConfigScreen extends Screen {
         } catch (Exception e) {
             valid = false;
             Taffeta.LOGGER.error("Failed to update config");
+            Taffeta.LOGGER.error(e);
+            CoolConfigNt.load(modConfig, knownGood);
+            data = CoolConfigNt.save(modConfig);
         }
         if (valid) {
             modConfig.save();
@@ -66,22 +104,100 @@ public class ConfigScreen extends Screen {
         }
 
         @Override
+        protected int getScrollbarPositionX() {
+            return width - 5;
+        }
+
+        @Override
         public int getRowWidth() {
             return Math.min(width - 40, 700);
         }
         
     }
 
-    private abstract class Entry extends EntryListWidget.Entry<Entry> { }
+    private abstract class Entry extends EntryListWidget.Entry<Entry> {
+        int indent = 0;
+    }
+
+    private class MapHeadEntry extends Entry {
+        private final String key;
+        private final Map<String, NestedTextNode> parent;
+
+        public MapHeadEntry(String key, Map<String, NestedTextNode> parent) {
+            this.key = key;
+            this.parent = parent;
+        }
+
+        @Override
+        public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
+            client.textRenderer.draw(matrices, key, (x + (indent * 32)), y, 14737632);
+        }
+    }
+
+    private class AppendMapButton extends Entry {
+        private final Map<String, NestedTextNode> parent;
+        private final ButtonWidget buttonWidget;
+
+        public AppendMapButton(Map<String, NestedTextNode> parent) {
+            this.parent = parent;
+            this.buttonWidget = new ButtonWidget(0, 0, 20, 20, new LiteralText("+"), b -> {
+                parent.put("", NestedTextNode.of(""));
+                rebuild();
+            });
+            children.add(buttonWidget);
+        }
+
+        @Override
+        public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
+            buttonWidget.x = x + Math.min(width - 40, 700) - 100;
+            buttonWidget.y = y;
+            buttonWidget.render(matrices, mouseX, mouseY, tickDelta);
+        }
+    }
+
+    private class KVEntry extends Entry {
+        private String[] key = new String[1];
+        private final Map<String, NestedTextNode> parent;
+        private final TextFieldWidget keyw;
+        private final TextFieldWidget value;
+
+        public KVEntry(String originalKey, Map<String, NestedTextNode> parent) {
+            this.key[0] = originalKey;
+            this.parent = parent;
+            this.value = new TextFieldWidget(textRenderer, 0, 0, 100, 20, LiteralText.EMPTY);
+            this.value.setText(this.parent.get(key[0]).asLeafString());
+            this.value.setChangedListener(newString -> this.parent.put(this.key[0], NestedTextNode.of(newString)));
+            children.add(this.value);
+            this.keyw = new TextFieldWidget(textRenderer, 0, 0, 100, 20, LiteralText.EMPTY);
+            this.keyw.setText(key[0]);
+            this.keyw.setChangedListener(newString -> {
+                NestedTextNode valueNode = parent.get(this.key[0]);
+                parent.remove(this.key[0]);
+                parent.put(newString, valueNode);
+                this.key[0] = newString;
+            });
+            children.add(this.keyw);
+        }
+
+        @Override
+        public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
+            keyw.x = x + (indent * 32);
+            keyw.y = y;
+            keyw.render(matrices, mouseX, mouseY, tickDelta);
+            value.x = x + Math.min(width - 40, 700) - 100;
+            value.y = y;
+            value.render(matrices, mouseX, mouseY, tickDelta);
+        }
+    }
 
     private class LeafEntry extends Entry {
         private final String key;
         private final Map<String, NestedTextNode> parent;
         private final TextFieldWidget textFieldWidget;
 
-        public LeafEntry(String key, NestedTextNode parent) {
+        public LeafEntry(String key, Map<String, NestedTextNode> parent) {
             this.key = key;
-            this.parent = parent.asMap();
+            this.parent = parent;
             this.textFieldWidget = new TextFieldWidget(textRenderer, 0, 0, 100, 20, LiteralText.EMPTY);
             this.textFieldWidget.setText(this.parent.get(key).asLeafString());
             this.textFieldWidget.setChangedListener(newString -> this.parent.put(key, NestedTextNode.of(newString)));
@@ -90,7 +206,7 @@ public class ConfigScreen extends Screen {
 
         @Override
         public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
-            client.textRenderer.draw(matrices, key, x, y, 14737632);
+            client.textRenderer.draw(matrices, key, (x + (indent * 32)), y, 14737632);
             textFieldWidget.x = x + Math.min(width - 40, 700) - 100;
             textFieldWidget.y = y;
             textFieldWidget.render(matrices, mouseX, mouseY, tickDelta);
--- src/testmod/java/io/github/thecursedfabricproject/taffeta/test/config/ConfigTestMod.java
+++ src/testmod/java/io/github/thecursedfabricproject/taffeta/test/config/ConfigTestMod.java
@@ -1,5 +1,8 @@
 package io.github.thecursedfabricproject.taffeta.test.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.github.coolmineman.coolconfig.annotation.Comment;
 import io.github.thecursedfabricproject.taffeta.config.ModConfig;
 import io.github.thecursedfabricproject.taffeta.config.TaffetaConfig;
@@ -18,6 +21,10 @@ public class ConfigTestMod implements ModInitializer {
         @Comment("The epic amount of this config")
         default int epic_amount() {
             return 100;
+        }
+
+        default Map<Integer, String> ir() {
+            return Map.of(8, "bruh");
         }
     }
 }
